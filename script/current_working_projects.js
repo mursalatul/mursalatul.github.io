40bee1bc02d210119f0069730eb4f54a76c8f6d4
mursalatul <md.mursalatulislampallob@gmail.com>
2021-07-30 05:23:53 -0400

    modified about section

--- script/current_working_projects.js
+++
@@ -1,4 +0,0 @@
-// 
-// this script file will show current working projects. it will be gitignore cause this will change many times (cause current project changes). but codes are important, so that current project relaed code are in also Project class in main script file.
-// 
-
