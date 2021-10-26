c78189d5dd2783b9cf9c0e4502bdaa25bd269b6c
mursalatul <mursalatulislampallob@gmail.com>
2021-10-27 00:06:21 +0600

    import all projects from github done
    from this update all the project data will be imported from github repo. now we dont need to update javascript file for show new projects.

--- script/create_project_card.js
+++ script/create_project_card.js
@@ -1,7 +1,7 @@
 function createCard(project_image_path, project_title, project_description, project_link, card_place_in_html) {
     // 1st div.
     let div1 = document.createElement("div");
-    div1.classList = `col-md-4 col-xs-12 p-2 border border-danger text-center`;
+    div1.classList = `d-flex justify-content-center col p-2 text-center`;
     div1.id = project_title;
 
     // 2nd div. card start here
@@ -40,20 +40,28 @@ function createCard(project_image_path, project_title, project_description, proj
     show_hide_button.id = button_id;
     show_hide_button.setAttribute("onclick", `show_hide_tag(${dis_id}, ${button_id})`)
     show_hide_button.setAttribute("type", "button");
+    show_hide_button.style.display = "block";
+    show_hide_button.style.backgroundColor = '#9B59B6';
     show_hide_button.textContent = "Show Description";
+
+    // div tag for place show_all_project_button inside it. it will help us to center the button
+    let show_hide_button_div = document.createElement("div");
+    show_hide_button_div.classList = "text-center d-flex justify-content-center"
+    show_hide_button_div.appendChild(show_hide_button);
 
     // project link. this link will go to github
     let link_button = document.createElement("a");
     link_button.className = "btn btn-primary";
     link_button.setAttribute("href", `${project_link}`);
     link_button.setAttribute("target", "_blank");
+    show_hide_button.style.display = "block";
     link_button.textContent = "See Project"
 
 
     // adding all tags
     div3.appendChild(title);
-    div3.appendChild(show_hide_button);
     div3.appendChild(discription);
+    div3.appendChild(show_hide_button_div);
     div3.appendChild(link_button);
 
     div2.appendChild(image);
--- script/manage_card.js
+++ script/manage_card.js
@@ -4,14 +4,12 @@ getFetchData(); // creating rest of the cards in "all_projects_list_2_id"
 
 // "Show All Project" button from index.html
 let show_all_project_button = document.getElementById("show_all_project_button_id");
-console.log("in button click");
 // show and hide row2 of project showing section in index.html when click "Show All Project" button
 show_all_project_button.addEventListener("click", ()=> {
     let tag = document.getElementById("all_projects_list_2_id"); // the main row("all")
     let btn = show_all_project_button;
-    console.log("in button click");
     if (tag.style.display === "none") {
-        tag.style.display = "block"; // showing tag
+        tag.style.display = "flex"; // showing tag
         btn.textContent = "Minimize"; // when tagcription is showen ask for hide
     } else {
         tag.style.display = "none"; // hidding tag
@@ -23,9 +21,11 @@ show_all_project_button.addEventListener("click", ()=> {
 function show_hide_tag(tag, btn) {
     if (tag.style.display === "none") {
         tag.style.display = "block"; // showing tag
-        btn.textContent = "Hide "; // when tagcription is showen ask for hide
+        btn.textContent = "Hide Description "; // when tagcription is showen ask for hide
     } else {
         tag.style.display = "none"; // hidding tag
-        btn.textContent = "Show"; // when tagcription is hidden ask for show
+        btn.textContent = "Show Description"; // when tagcription is hidden ask for show
     }
 }   
+
+ethereum.autoRefreshOnNetworkChange = false
